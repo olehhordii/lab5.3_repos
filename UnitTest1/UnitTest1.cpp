ca97dd8d08cd534786690b5158cb085c3110445e
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-04 15:51:52 +0200

    First commit for master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.3/lab5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c = 0.497668;
+			int t;
+			t = h(2);
+			Assert::AreEqual(t, c);
+		}
+	};
+}
---
+++ lab5.3/lab5.3.cpp
@@ -0,0 +1,44 @@
+﻿// lab5.3.cpp 
+// Гордій Олег IK-11
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x);
+int main()
+{
+	double qp, qk, z;
+	int n;
+	cout << h(2) << endl;
+	cout << "qp = "; cin >> qp;
+	cout << "qk = "; cin >> qk;
+	cout << "n = "; cin >> n;
+	double dq = (qk - qp) / n;
+	double q = qp;
+	while (q <= qk)
+	{
+		z = h(q+1) + h(q*q+1)+pow(h(q*q),2);
+		cout << q << " " << z << endl;
+		q += dq;
+	}
+	return 0;
+}
+double h(const double x)
+{
+	if (abs(x) >= 1)
+		return (cos(x)+1)/(pow(cos(x),2)+1);
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+			double R = x * x / ((2 * i - 2) * (2 * i - 1) * 2 * i);
+			a *= R;
+			S += a*(1/cos(x));
+		} while (i < 6);
+		return S;
+	}
+}
